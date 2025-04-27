b4e9940f3f0425203e410a530238de27d9ec9527
devFlashcap <dev.flashcap@gmail.com>
2025-04-27 03:30:52 +0200

    Lsp config updates and formatting

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,32 +1,77 @@
-local diagnostic_config = function()
-    local x = vim.diagnostic.severity
+---------- LSP CONFIG ----------
 
-    vim.diagnostic.config {
-        virtual_text = { prefix = "" },
-        signs = { text = { [x.ERROR] = "󰅙", [x.WARN] = "", [x.INFO] = "󰋼", [x.HINT] = "󰌵" } },
-        underline = true,
-        float = { border = "single" },
-    }
+local function keymap(mode, l, r, opts)
+	opts = opts or {}
+	opts.buffer = true
+	opts.desc = string.format("Lsp: %s", opts.desc)
+	vim.keymap.set(mode, l, r, opts)
 end
 
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("DefaultLspAttach", { clear = true }),
+	callback = function()
+		keymap("n", "gh", function()
+			vim.lsp.buf.hover({})
+		end, { desc = "Hover" })
+
+		keymap("n", "<leader>th", function()
+			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+		end, { desc = "Toggle inlay hints" })
+
+		keymap("n", "<leader>gd", function()
+			vim.lsp.buf.definition()
+		end, { desc = "Go to definition" })
+
+		keymap("n", "<leader>gi", function()
+			vim.lsp.buf.implementation()
+		end, { desc = "Go to implementation" })
+
+		keymap("n", "<leader>sh", function()
+			vim.lsp.buf.signature_help()
+		end, { desc = "Signature help" })
+
+		keymap("n", "<leader>ca", function()
+			vim.lsp.buf.code_action()
+		end, { desc = "Code action" })
+
+		keymap("n", "<leader>ff", function()
+			vim.lsp.buf.format()
+		end, { desc = "Code action" })
+	end,
+})
+
+vim.diagnostic.config({
+	virtual_text = { spacing = 4, prefix = "●" },
+	---@diagnostic disable-next-line: assign-type-mismatch
+	float = { source = "if_many" },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "✘",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.HINT] = "",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+			[vim.diagnostic.severity.INFO] = "DiagnosticSignHint",
+		},
+	},
+})
+
 return {
-    "williamboman/mason-lspconfig.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-	"neovim/nvim-lspconfig",
-	"williamboman/mason.nvim",
-	"saghen/blink.cmp"
-    },
-    config = function()
-        diagnostic_config()
-	require("mason-lspconfig").setup({
-	    handlers = {
-		function(server)
-		    print(server)
-		    vim.lsp.config(server, require("blink.cmp").get_lsp_capabilities())
-		    vim.lsp.enable(server)
-		end
-	    }
-	})
-    end
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			handlers = {
+				function(server)
+					vim.lsp.enable(server)
+				end,
+			},
+		},
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim", opts = {}, cmd = "Mason", dependencies = { "roslyn.nvim" } },
+		},
+	},
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,20 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.smarttab = true
-vim.opt.shiftwidth = 4
 vim.g.mapleader = " "
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+vim.keymap.set({ 'n', 'i', 'v' }, '<Up>', function()
+	print("NO! Use k")
+end)
+vim.keymap.set({ 'n', 'i', 'v' }, '<Down>', function()
+	print("NO! Use j")
+end)
+vim.keymap.set({ 'n', 'i', 'v' }, '<Left>', function()
+	print("NO! Use h")
+end)
+vim.keymap.set({ 'n', 'i', 'v' }, '<Right>', function()
+	print("NO! Use l")
+end)
